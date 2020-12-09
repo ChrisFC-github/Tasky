4606b533b13dc2b61e2949593bae86b5dd7e6387
Niki Beck <Niki@wearehamburger.com>
2020-12-09 09:42:06 -0600

    Styling is finished. Just waiting on the reload code.

--- src/client/components/Dashboard/Reminders/TaskCard.tsx
+++ src/client/components/Dashboard/Reminders/TaskCard.tsx
@@ -2,15 +2,16 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { useHistory, RouteComponentProps, useParams } from 'react-router-dom';
 import EditReminder from './EditReminders';
+import moment from 'moment';
 
 const TaskCard: React.FC<IEvents> = (props) => {
 
 
 
     return (
-        
+      
             <li className="task-list-group-item"> 
-            {props.events.title}
+            {props.events.title}: {moment(props.events.date).add(0, 'days').calendar()}
             </li>
       
     )
